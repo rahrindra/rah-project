1a728dd7c22ceb2ece883308e02c9ade9559ef9a
Rindra <rahrindra@gmail.com>
2021-02-15 18:33:16 +0100

    complete fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -27,10 +27,27 @@ class AppFixtures extends Fixture
     {
         $user = new User();
         $user->setEmail('user@gmail.com');
-        $password = $this->encoder->encodePassword($user, 'pass');
-        $user->setPassword($password);
+        $user->setRoles(['ROLE_USER']);
+        $userPassword = $this->encoder->encodePassword($user, 'pass');
+        $user->setPassword($userPassword);
+
+        // le commerçant
+        $partner = new User();
+        $partner->setEmail('partner@gmail.com');
+        $partner->setRoles(['ROLE_PARTNER']);
+        $partnerPassword = $this->encoder->encodePassword($partner, 'pass');
+        $partner->setPassword($partnerPassword);
+
+        // l'admininstrateur RAH
+        $admin = new User();
+        $admin->setEmail('admin@gmail.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $adminPassword = $this->encoder->encodePassword($admin, 'pass');
+        $admin->setPassword($adminPassword);
 
         $manager->persist($user);
+        $manager->persist($partner);
+        $manager->persist($admin);
         $manager->flush();
     }
 }
